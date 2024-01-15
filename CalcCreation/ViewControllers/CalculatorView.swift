6a957b6cc61f8342eed726cb8efac2782d38c773
Rattonikus <71366308+Rattonikus@users.noreply.github.com>
2024-01-15 00:27:46 -0700

    Cleaned up calc view further

--- CalcCreation/ViewControllers/CalculatorView.swift
+++ CalcCreation/ViewControllers/CalculatorView.swift
@@ -17,25 +17,9 @@ struct CalculatorView: View
                 .edgesIgnoringSafeArea(.all)
             VStack
             {
-                Text(displayValue)
-                    .foregroundColor(.white)
-                //Hope to remove this with the buttonstyles swift file. 
-                ForEach(viewButtons, id: \.self)
-                {
-                    row in
-                    //This will iterate over the buttons, and assign them to row i think.
-                    HStack
-                    {
-                        ForEach(row, id: \.self)
-                        {
-                            item in
-                            
-                            Button(action: {buttonPressed(button: item)}, label: {Text(item.rawValue)})
-                            
-                        }
-                    }
-                    
-                }
+                displayText
+                buttonPad
+                //Cleaned up using extentions. 
             }
         .padding()
         }
@@ -47,4 +31,43 @@ struct CalculatorView: View
 #Preview 
 {
     CalculatorView()
+}
+
+// MARK: COMPONENTS
+
+extension CalculatorView
+{
+    private var displayText: some View
+    {
+        Text(displayValue)
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .foregroundColor(.white)
+            .font(.system(size: 88, weight: .light))
+            .lineLimit(1)
+        //This stops it from going above, just like in iOS
+            .minimumScaleFactor(0.2)
+        //This shrinks the text when it becomes too large for the display.
+    }
+    
+    private var buttonPad: some View
+    {
+        ForEach(viewButtons, id: \.self)
+        {
+            row in
+            //This will iterate over the buttons, and assign them to row i think.
+            HStack
+            {
+                ForEach(row, id: \.self)
+                {
+                    item in
+                    
+                    Button(action: {buttonPressed(button: item)}, label: {Text(item.rawValue)})
+                    
+                }
+            }
+        }
+    }
+    
+    
 }
